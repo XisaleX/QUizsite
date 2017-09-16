93176e84e18f848cc174eba869df3edece48c8b2
XisaleX <megasxlr14@gmail.com>
2017-09-16 07:19:01 +0000

    header page

---
+++ app/controllers/quizzer_controller.rb
@@ -0,0 +1,6 @@
+class QuizzerController < ApplicationController
+  def index
+    
+  end
+
+end
---
+++ test/controllers/quizzer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuizzerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
